08739ad990fcc50322180f1922624249cff89866
Radhwanetouahria <167095504+Radhwanetouahria@users.noreply.github.com>
2026-04-25 16:24:52 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /****************** Typing Animation ****************/
 
-var typed = new Typed('.typing', {
-    strings: ['', 'Web Developer', 'Web Designer', 'Sponsored Ads', 'YouTuber'],
+var tsyped = new Typed('.typing', {
+    strings: ['', 'Geodesy', 'Photogrammetry', 'Remote Sensing', 'Geographic Information Systems'],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true,
@@ -45,7 +45,7 @@ function showSection(element) {
 
 /************** Nav Toggler **************/
 function updateNav(element) {
-    for (let i = 0; i < totalNavList; i++){
+    for (let i = 0; i < totalNavList; i++) {
         navList[i].querySelector('a').classList.remove('active');
         const target = element.getAttribute('href').split('#')[1];
         if (target === navList[i].querySelector('a').getAttribute('href').split('#')[1]) {
@@ -63,13 +63,41 @@ document.querySelector('.hire-me').addEventListener('click', function () {
 
 const navTogglerBtn = document.querySelector('.nav-toggle'),
     aside = document.querySelector('.aside');
-    navTogglerBtn.addEventListener('click', () => {
-        asideSectionTogglerBtn();
-    })
-    function asideSectionTogglerBtn() {
-        aside.classList.toggle('open');
-        navTogglerBtn.classList.toggle('open');
-        // for (let i = 0; i < totalSection; i++){
-        //     allSection[i].classList.toggle('open');
-        // }
+navTogglerBtn.addEventListener('click', () => {
+    asideSectionTogglerBtn();
+})
+function asideSectionTogglerBtn() {
+    aside.classList.toggle('open');
+    navTogglerBtn.classList.toggle('open');
+// for (let i = 0; i < totalSection; i++){
+    //     allSection[i].classList.toggle('open');
+    // }
+}
+
+/************** Portfolio Lightbox **************/
+const portfolioItems = document.querySelectorAll('.portfolio-item-inner');
+const lightbox = document.createElement('div');
+lightbox.className = 'lightbox';
+lightbox.innerHTML = '<div class="lightbox-content"><img src="" class="lightbox-img"><span class="lightbox-close">&times;</span></div>';
+document.body.appendChild(lightbox);
+
+const lightboxImg = lightbox.querySelector('.lightbox-img');
+const lightboxClose = lightbox.querySelector('.lightbox-close');
+
+portfolioItems.forEach(item => {
+    item.addEventListener('click', function() {
+        const imgSrc = this.querySelector('.portfolio-img img').getAttribute('src');
+        lightboxImg.setAttribute('src', imgSrc);
+        lightbox.classList.add('open');
+    });
+});
+
+lightboxClose.addEventListener('click', () => {
+    lightbox.classList.remove('open');
+});
+
+lightbox.addEventListener('click', (e) => {
+    if (e.target === lightbox) {
+        lightbox.classList.remove('open');
     }
+});
